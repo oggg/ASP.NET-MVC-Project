30141c422c0ca6118eac576720f871c549bf9bc9
oggg <ogggster@gmail.com>
2016-02-11 11:42:17 +0200

    RouteConfig.cs added namespaces parameter

--- ApplicationStore/Web/ApplicationStore.Web/Areas/Administration/AdministrationAreaRegistration.cs
+++ ApplicationStore/Web/ApplicationStore.Web/Areas/Administration/AdministrationAreaRegistration.cs
@@ -2,20 +2,20 @@
 
 namespace ApplicationStore.Web.Areas.Administration
 {
-    public class AdministrationAreaRegistration : AreaRegistration 
+    public class AdministrationAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Administration";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
-                "Administration_default",
+                "Administration",
                 "Administration/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
             );
