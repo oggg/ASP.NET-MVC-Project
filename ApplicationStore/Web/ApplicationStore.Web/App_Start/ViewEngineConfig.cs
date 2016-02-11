0db97b756dfc76e3d54c1424d71b8e94d84ae75d
oggg <ogggster@gmail.com>
2016-02-11 11:19:19 +0200

    areas added

---
+++ ApplicationStore/Web/ApplicationStore.Web/App_Start/ViewEngineConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ApplicationStore.Web.App_Start
+{
+    public class ViewEngineConfig
+    {
+        public static void RegisterViewEngines(ViewEngineCollection viewEngines)
+        {
+            viewEngines.Clear();
+            viewEngines.Add(new RazorViewEngine());
+        }
+    }
+}
